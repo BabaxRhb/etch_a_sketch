d7559bf5c083b2868c647ae26a693bb18e35eecb
baba.randrianarisaona <baba.randrianarisaona@gmail.com>
2026-02-18 21:18:35 +0300

    Adding hover

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 const container = document.querySelector("#container");
 
-const grid = document.createElement("div");
-grid.classList.add("grid");
-container.appendChild(grid);
-
-const createBox = () => {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    grid.appendChild(box);
-}
-
 const createGrid = (row, col) => {
-    for(let j = 0; j < col; j++)
-    {
-        for (let i = 0; i < row; i++)
-            createBox();
+    for (let j = 0; j < col; j++) {
+        const rowGrid = document.createElement("div");
+        rowGrid.classList.add("row");
+        container.appendChild(rowGrid);
+        for (let i = 0; i < row; i++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            rowGrid.appendChild(box);
+        }
     }
 }
 
-createGrid(4, 4);
+const row = 4;
+const column = 4;
+
+createGrid(row, column);
